f48e2cce7557eb8024076e421bf72dfff70597a9
SiderealStorm <112557944+SiderealStorm@users.noreply.github.com>
2023-02-14 12:24:17 +0100

    gitignored application.properties

--- src/main/java/com/example/prospects/repository/ClientsRepo.java
+++ src/main/java/com/example/prospects/repository/ClientsRepo.java
@@ -1,5 +1,7 @@
 package com.example.prospects.repository;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.example.prospects.model.Client;
 @Repository
 public interface ClientsRepo extends JpaRepository<Client, Integer> {
 
+	ArrayList<Client> getAllClients();
 }
